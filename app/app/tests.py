1d53736aa75a924d61fb1061a4437c144d4cb51b
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-12-09 22:25:44 +0530

    added user model with tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,19 +5,20 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """
-        Test the calc module.
+    Test the calc module.
     """
 
     def test_add_numbers(self):
         """Test adding two numbers together."""
-        res = calc.add(5,7)
+        res = calc.add(5, 7)
 
         self.assertEqual(res, 12)
 
     def test_subtract_numbers(self):
         """Test subtract two numbers together."""
-        res = calc.subtract(6,6)
+        res = calc.subtract(6, 6)
 
         self.assertEqual(res, 0)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,8 +4,8 @@ Django command to wait for the database to be available.
 
 import time
 
-from psycopg2 import OperationalError as Psycopg2Error # type: ignore
 
+from psycopg2 import OperationalError as Psycopg2Error  # type: ignore
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
